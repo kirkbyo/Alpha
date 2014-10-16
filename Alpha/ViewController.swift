6487e9bb392be7401db00c2bda9c124d4ce1328b
kirkbyo <ozzie@firespread.co>
2014-10-16 18:54:54 -0400

    Initial Commit

---
+++ Alpha/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Alpha
+//
+//  Created by Ozzie Kirkby on 2014-10-16.
+//  Copyright (c) 2014 Ozzie Kirkby. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
