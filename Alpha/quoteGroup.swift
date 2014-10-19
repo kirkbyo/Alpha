7936c7a197c0d546c8d714dd54925e725647cd79
kirkbyo <ozzie@firespread.co>
2014-10-19 19:07:01 -0400

    Added All Fortunes

---
+++ Alpha/quoteGroup.swift
@@ -0,0 +1,29 @@
+//
+//  quoteGroup.swift
+//  Alpha
+//
+//  Created by Ozzie Kirkby on 2014-10-19.
+//  Copyright (c) 2014 Ozzie Kirkby. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+/*
+struct quoteGroup {
+    
+    let quoteDictionary = [
+        "Author 1": "Quote 1",
+        "Author 2": "Quote 2",
+        "Author 3": "Quote 3"
+    ]
+    
+    func randomFortune() -> String {
+        var unsignedArrayCount = UInt32(quoteDictionary.count)
+        var unsignedRandomNumber = arc4random_uniform(unsignedArrayCount)
+        var randomNumber = Int(unsignedRandomNumber)
+        
+        return quoteDictionary[randomNumber]
+    }
+}
+*/
---
+++ Alpha/quotes.swift
@@ -0,0 +1,26 @@
+//
+//  quotes.swift
+//  Alpha
+//
+//  Created by Ozzie Kirkby on 2014-10-19.
+//  Copyright (c) 2014 Ozzie Kirkby. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class quotes: UIViewController {
+    
+    @IBOutlet weak var quoteDisplay: UILabel!
+    @IBOutlet weak var authorDisplay: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+    
+    
+    @IBAction func newQuote() {
+        
+    }
+}
