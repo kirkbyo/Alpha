1d8dcad78421797b36ba5599a996766075687937
kirkbyo <ozzie@firespread.co>
2014-11-15 12:52:54 -0500

    Random Quote Functions Working

--- Alpha/quotes.swift
+++ Alpha/quotes.swift
@@ -14,13 +14,33 @@ class quotes: UIViewController {
     @IBOutlet weak var quoteDisplay: UILabel!
     @IBOutlet weak var authorDisplay: UILabel!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-       
+    let quoteArray = [
+        ["Quote 1", "Author 1"],
+        ["Quote 2", "Author 2"],
+        ["Quote 3", "Author 3"],
+        ["Quote 4", "Author 4"]
+    ]
+    
+    func getQuote() {
+        var quoteIndex = quoteArray[randomNumber(quoteArray.count)]
+        quoteDisplay.text = quoteIndex[0]
+        authorDisplay.text = quoteIndex[1]
     }
     
+    func randomNumber(arrayLength: Int) -> Int {
+        var unsignedArrayCount = UInt32(arrayLength)
+        var unsignedRandomNumber = arc4random_uniform(unsignedArrayCount)
+        var randomNumber = Int(unsignedRandomNumber)
+        
+        return randomNumber
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getQuote()
+    }
     
     @IBAction func newQuote() {
-        
+        getQuote()
     }
 }
