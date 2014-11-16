9333f73331d282696dbd43694449c4cc3aa62b1e
kirkbyo <ozzie@firespread.co>
2014-11-15 22:11:49 -0500

    Add Random Quotes

--- Alpha/quotes.swift
+++ Alpha/quotes.swift
@@ -14,33 +14,38 @@ class quotes: UIViewController {
     @IBOutlet weak var quoteDisplay: UILabel!
     @IBOutlet weak var authorDisplay: UILabel!
     
-    let quoteArray = [
-        ["Quote 1", "Author 1"],
-        ["Quote 2", "Author 2"],
-        ["Quote 3", "Author 3"],
-        ["Quote 4", "Author 4"]
-    ]
-    
-    func getQuote() {
-        var quoteIndex = quoteArray[randomNumber(quoteArray.count)]
-        quoteDisplay.text = quoteIndex[0]
-        authorDisplay.text = quoteIndex[1]
-    }
-    
-    func randomNumber(arrayLength: Int) -> Int {
-        var unsignedArrayCount = UInt32(arrayLength)
-        var unsignedRandomNumber = arc4random_uniform(unsignedArrayCount)
-        var randomNumber = Int(unsignedRandomNumber)
-        
-        return randomNumber
-    }
+    var Author : String = ""
+    var Quote : String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        getQuote()
     }
     
     @IBAction func newQuote() {
-        getQuote()
+        
+        func randomNumber(arrayLength: Int) -> Int {
+            var unsignedArrayCount = UInt32(arrayLength)
+            var unsignedRandomNumber = arc4random_uniform(unsignedArrayCount)
+            var randomNumber = Int(unsignedRandomNumber)
+            
+            return randomNumber
+        }
+        
+        // Importing Quotes plist File
+        let quotePath = NSBundle.mainBundle().pathForResource("QuotesList", ofType: "plist")
+        let dictionary: NSDictionary = NSDictionary(contentsOfFile: quotePath!)!
+        let quoteDictionary: Dictionary = dictionary
+        let quoteStringArray = [String](map(quoteDictionary.keys) { $0 as AnyObject as String })
+        
+        // Selects Quote
+        let chosenQuote = quoteStringArray[randomNumber(quoteDictionary.count)]
+        let chosenAuthor = quoteDictionary[chosenQuote] as NSString as String
+        
+        // Assigns Quote & Author to IBOutlet
+        Author = chosenAuthor
+        Quote = chosenQuote
+        
+        quoteDisplay.text = Quote
+        authorDisplay.text = Author
     }
 }
